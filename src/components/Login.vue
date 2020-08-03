a6112b34831b275a16cf7a6c3f40d6f24b2328ce
limeihua-dev <1344489235@qq.com>
2020-08-03 16:25:06 +0800

    add login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,36 @@
 <template>
-  <h1> 登陆页</h1>
+  <div>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="账号">
+        <el-input v-model="form.name" placeholder="请输入您的账号"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password" placeholder="请输入您的密码"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "Home"
-}
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+        password: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        alert('submit!');
+      }
+    }
+  }
 </script>
 
 <style scoped>
